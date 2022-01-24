ecb76d6bb7c80c240138d8e11c45ea078073fedd
João Pedro Gama F. Mattos <jopedrogama@gmail.com>
2022-01-24 19:28:22 -0300

    update api for cep - brazilian zip code-

--- logistica-api/src/main/java/com/api/logisticaapi/Domain/Services/RequestDeliveryService.java
+++ logistica-api/src/main/java/com/api/logisticaapi/Domain/Services/RequestDeliveryService.java
@@ -2,13 +2,16 @@ package com.api.logisticaapi.Domain.Services;
 
 import java.time.OffsetDateTime;
 
+import com.api.logisticaapi.Domain.Exceptions.DomainException;
 import com.api.logisticaapi.Domain.Models.Delivery;
 import com.api.logisticaapi.Domain.Models.DeliveryStatus;
 import com.api.logisticaapi.Domain.Repositories.DeliveryRepository;
+import com.api.logisticaapi.Dtos.ConsumingApiDTO.AddressDTO;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class RequestDeliveryService {
@@ -27,7 +30,20 @@ public class RequestDeliveryService {
         delivery.setDelivery_status(DeliveryStatus.PENDING);
         delivery.setOrder_date(OffsetDateTime.now());
         delivery.setCustomer(customer);
-        // CEP pesquisa e seta valores
+
+        try {
+            RestTemplate template = new RestTemplate();
+            String cep = delivery.getRecipient().getCep().replaceAll("-", "");
+            String url = "https://viacep.com.br/ws/" + cep + "/json/";
+            AddressDTO queryAddress = template.getForObject(url, AddressDTO.class);
+            delivery.getRecipient().setStreet(queryAddress.getLogradouro());
+            delivery.getRecipient().setDistrict(queryAddress.getBairro());
+            delivery.getRecipient().setCity(queryAddress.getLocalidade());
+            delivery.getRecipient().setState(queryAddress.getUf());
+
+        } catch (Exception e) {
+            throw new DomainException("This CEP is invalid! Try a different one!");
+        }
 
         return deliveryRepository.save(delivery);
     }
--- logistica-api/src/main/java/com/api/logisticaapi/Mapper/DeliveryMapper.java
+++ logistica-api/src/main/java/com/api/logisticaapi/Mapper/DeliveryMapper.java
@@ -3,16 +3,13 @@ package com.api.logisticaapi.Mapper;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import com.api.logisticaapi.Domain.Exceptions.DomainException;
 import com.api.logisticaapi.Domain.Models.Delivery;
-import com.api.logisticaapi.Dtos.ConsumingApiDTO.AddressDTO;
 import com.api.logisticaapi.Dtos.Request.DeliveryRequest;
 import com.api.logisticaapi.Dtos.Response.DeliveryDTO;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 @Component
 public class DeliveryMapper {
@@ -30,19 +27,6 @@ public class DeliveryMapper {
 
     public Delivery toEntity(DeliveryRequest deliveryInput) {
         Delivery delivery = modelMapper.map(deliveryInput, Delivery.class);
-        try {
-            RestTemplate template = new RestTemplate();
-            String cep = delivery.getRecipient().getCep().replaceAll("-", "");
-            String url = "https://viacep.com.br/ws/" + cep + "/json/";
-            AddressDTO queryAddress = template.getForObject(url, AddressDTO.class);
-            delivery.getRecipient().setStreet(queryAddress.getLogradouro());
-            delivery.getRecipient().setDistrict(queryAddress.getBairro());
-            delivery.getRecipient().setCity(queryAddress.getLocalidade());
-            delivery.getRecipient().setState(queryAddress.getUf());
-
-        } catch (Exception e) {
-            throw new DomainException("This CEP is invalid! Try a different one!");
-        }
         return delivery;
     }
 }
